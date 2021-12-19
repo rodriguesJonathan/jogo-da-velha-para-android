77630c6348c42c33ecc268f54a8bd1c64bbb1f26
Jonathan Rodrigues <jonathanlr2000@gmail.com>
2021-12-19 01:37:34 -0300

    Add actions

--- app/src/main/java/com/example/jogodavelha/MainActivity.java
+++ app/src/main/java/com/example/jogodavelha/MainActivity.java
@@ -4,17 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    int screenWidth;
+    private char letraGlobalAtual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        this.letraGlobalAtual = 'x';
     }
+
+    public void inserirXO(View view){
+        SquareXorOView image = findViewById(view.getId());
+        if (image.getLetraAtual() == '_'){
+            image.setLetraAtual(this.letraGlobalAtual);
+            if (this.letraGlobalAtual == 'x'){
+                image.setImageResource(R.drawable.letter_x);
+            }else{
+                image.setImageResource(R.drawable.letter_o);
+            }
+            this.letraGlobalAtual = this.letraGlobalAtual == 'x' ? 'o' : 'x';
+        }
+    }
+
+    public void reiniciarJogo (View view){
+        SquareXorOView tabela11 = findViewById(R.id.tabela11);
+        SquareXorOView tabela12 = findViewById(R.id.tabela12);
+        SquareXorOView tabela13 = findViewById(R.id.tabela13);
+        SquareXorOView tabela21 = findViewById(R.id.tabela21);
+        SquareXorOView tabela22 = findViewById(R.id.tabela22);
+        SquareXorOView tabela23 = findViewById(R.id.tabela23);
+        SquareXorOView tabela31 = findViewById(R.id.tabela31);
+        SquareXorOView tabela32 = findViewById(R.id.tabela32);
+        SquareXorOView tabela33 = findViewById(R.id.tabela33);
+
+        SquareXorOView camposTabela[] = {tabela11, tabela12, tabela13, tabela21, tabela22, tabela23, tabela31, tabela32, tabela33};
+        for (SquareXorOView campo : camposTabela) {
+            campo.setImageResource(R.drawable.vazio);
+            campo.setLetraAtual('_');
+        }
+
+        this.letraGlobalAtual = 'x';
+    }
+
 }
--- app/src/main/java/com/example/jogodavelha/SquareXorOView.java
+++ app/src/main/java/com/example/jogodavelha/SquareXorOView.java
@@ -2,29 +2,36 @@ package com.example.jogodavelha;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.View;
 
-public class SquareXorOView extends androidx.appcompat.widget.AppCompatImageView {
-    DisplayMetrics displayMetrics;
+public class SquareXorOView extends androidx.appcompat.widget.AppCompatImageView implements Drawable.Callback {
+    private DisplayMetrics displayMetrics;
+    private char letraAtual;
 
     public SquareXorOView(Context context) {
         super(context);
         displayMetrics = new DisplayMetrics();
         ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        this.letraAtual = '_';
     }
 
     public SquareXorOView(Context context, AttributeSet attrs) {
         super(context, attrs);
         displayMetrics = new DisplayMetrics();
         ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        this.letraAtual = '_';
     }
 
     public SquareXorOView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         displayMetrics = new DisplayMetrics();
         ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        this.letraAtual = '_';
     }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -35,4 +42,15 @@ public class SquareXorOView extends androidx.appcompat.widget.AppCompatImageView
 
         setMeasuredDimension(width, width);
     }
+
+
+
+    public char getLetraAtual() {
+        return letraAtual;
+    }
+
+    public void setLetraAtual(char letraAtual) {
+        this.letraAtual = letraAtual;
+    }
+
 }
