1de4d3860eeb67a88632093bf9e7339cfccde90c
Jonathan Rodrigues <jonathanlr2000@gmail.com>
2022-02-03 23:00:36 -0300

    google auth problem

--- app/src/main/java/com/example/jogodavelha/OptionalFirstActivity.java
+++ app/src/main/java/com/example/jogodavelha/OptionalFirstActivity.java
@@ -1,10 +1,10 @@
 package com.example.jogodavelha;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,14 +15,19 @@ public class OptionalFirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_optional_first);
-        //Teste Firebase
+        /*
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
-        myRef.child("First child").setValue("Hello Word");
+        myRef.child("Second child").setValue("Hello Word");
+         */
     }
 
     public void openScreenOption1(View view){
         Intent intent = new Intent(getApplicationContext(), InputNameActivity.class);
+        startActivity(intent);
+    }
+    public void openScreenOption2(View view){
+        Intent intent = new Intent(getApplicationContext(), AuthActivity.class);
         startActivity(intent);
     }
 
